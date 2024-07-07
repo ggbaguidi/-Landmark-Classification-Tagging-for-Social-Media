d99c898f67c24dd65454dcc94e0dae63afc6f8d7
Ahonakpon Guy Gbaguidi <guygbaguidi123root@gmail.com>
2024-07-07 19:10:08 +0000

    feat: update and define MyModel architecture

--- src/model.py
+++ src/model.py
@@ -13,12 +13,65 @@ class MyModel(nn.Module):
         # to size appropriately the output of your classifier, and if you use
         # the Dropout layer, use the variable "dropout" to indicate how much
         # to use (like nn.Dropout(p=dropout))
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(512, 1024, 3, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+
+        self.head = nn.Sequential(
+            nn.Flatten(),
+            
+            nn.Linear(1024 * 7 * 7, 1024),
+            nn.BatchNorm1d(1024),
+            nn.Dropout(p=dropout),
+
+            nn.Linear(1024, num_classes)
+        )
+        
+        self.model = nn.Sequential(
+            self.layer1,
+            self.layer2,
+            self.layer3,
+            self.layer4,
+            self.layer5,
+            self.head,
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # YOUR CODE HERE: process the input tensor through the
         # feature extractor, the pooling and the final linear
         # layers (if appropriate for the architecture chosen)
-        return x
+        return self.model(x)
 
 
 ######################################################################################
