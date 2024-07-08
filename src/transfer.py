93f9a4265d204cbf15185b8a9ba63bd50057b665
Ahonakpon Guy Gbaguidi <guygbaguidi123root@gmail.com>
2024-07-08 11:31:20 +0000

    feat: update the transfer module

--- src/transfer.py
+++ src/transfer.py
@@ -8,30 +8,24 @@ def get_model_transfer_learning(model_name="resnet18", n_classes=50):
 
     # Get the requested architecture
     if hasattr(models, model_name):
-
         model_transfer = getattr(models, model_name)(pretrained=True)
-
     else:
-
         torchvision_major_minor = ".".join(torchvision.__version__.split(".")[:2])
-
         raise ValueError(f"Model {model_name} is not known. List of available models: "
                          f"https://pytorch.org/vision/{torchvision_major_minor}/models.html")
 
     # Freeze all parameters in the model
-    # HINT: loop over all parameters. If "param" is one parameter,
-    # "param.requires_grad = False" freezes it
-    # YOUR CODE HERE
+    for param in model_transfer.parameters():
+        param.requires_grad = False
 
-    # Add the linear layer at the end with the appropriate number of classes
-    # 1. get numbers of features extracted by the backbone
-    num_ftrs  = # YOUR CODE HERE
+    # Get the number of input features for the last fully connected layer
+    num_ftrs = model_transfer.fc.in_features
 
-    # 2. Create a new linear layer with the appropriate number of inputs and
-    #    outputs
-    model_transfer.fc  = # YOUR CODE HERE
+    # Replace the last fully connected layer with a new one (unfrozen by default)
+    model_transfer.fc = nn.Linear(num_ftrs, n_classes)
 
     return model_transfer
+
 
 
 ######################################################################################
